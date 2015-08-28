a3aed005a067c1b9e8512d4c30e33cedea266f78
Lauro Caetano <laurocaetano1@gmail.com>
2015-08-27 23:44:51 -0300

    Add minimal implementation to make the tests pass

--- lib/elixir_examples/list_operations.ex
+++ lib/elixir_examples/list_operations.ex
@@ -1,13 +1,31 @@
 defmodule ElixirExamples.ListOperations do
+  def sum([]) do
+    0
+  end
+
   def sum(list) do
-    # implementation goes here
+    list
+    |> Enum.reduce(fn(n, acc) -> acc + n end)
+  end
+
+  def product([]) do
+    1
   end
 
   def product(list) do
-    # implementation goes here
+    list
+    |> Enum.reduce(fn(n, acc) -> acc * n end)
   end
 
-  def all?(predicate, list) do
-    # implementation goes here
+  def all?(_, []) do
+    true
+  end
+
+  def all?(predicate, [x|xs]) do
+    if predicate.(x) do
+      all?(predicate, xs)
+    else
+      false
+    end
   end
 end
--- lib/elixir_examples/merge_sort.ex
+++ lib/elixir_examples/merge_sort.ex
@@ -1,5 +1,30 @@
 defmodule ElixirExamples.MergeSort do
+  def sort([]) do
+    []
+  end
+
+  def sort([element]) do
+    [element]
+  end
+
   def sort(list) do
-    # implementation goes here
+    {left, right} = Enum.split(list, div(length(list), 2))
+    merge(sort(left), sort(right))
+  end
+
+  defp merge([], y) do
+    y
+  end
+
+  defp merge(x, []) do
+    x
+  end
+
+  defp merge([x|xs], [y|ys]) do
+    if x <= y do
+      [x|merge(xs, [y|ys])]
+    else
+      [y|merge([x|xs], ys)]
+    end
   end
 end
