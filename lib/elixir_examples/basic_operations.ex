c933c3e360a8d190b71cef61d285cba8a738d885
Lauro Caetano <lauro.caetano@plataformatec.com.br>
2015-08-30 18:45:46 -0300

    Provide a minimum solution for the exercises

--- lib/elixir_examples/basic_operations.ex
+++ lib/elixir_examples/basic_operations.ex
@@ -1,13 +1,19 @@
 defmodule ElixirExamples.BasicOperations do
   def double(n) do
-    # Implementation goes here
+    n * 2
   end
 
   def sum(x, y) do
-    # Implementation goes here
+    x + y
   end
 
-  def list_length(list) do
-    # Implementation goes here
+  def list_length([]) do
+    0
+  end
+  def list_length([e]) do
+    1
+  end
+  def list_length([x|xs]) do
+    1 + list_length(xs)
   end
 end
